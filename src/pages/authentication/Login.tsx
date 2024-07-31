d6b23eb3373e27e0e3d80ddc092e5bc87fb46a08
Asaf Araujo <asaff.viana@outlook.com>
2024-07-31 17:58:57 -0300

    Shadcn/ui instalado e configurado

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -17,46 +17,55 @@ const Login: React.FC = () => {
 
   return (
     <>
-        <p className='font-bold w-full text-sm'>Bem vindo ao sistema administrativo!</p>
-        <p className='w-full text-sm'>Para iniciar o serviço faça seu login:</p>
-        <form onSubmit={handleSubmit(handleSignIn)} action=""
-            className='w-full flex flex-col gap-4 mt-4'
+      <p className="font-bold w-full text-sm">
+        Bem vindo ao sistema administrativo!
+      </p>
+      <p className="w-full text-sm">Para iniciar o serviço faça seu login:</p>
+      <form
+        onSubmit={handleSubmit(handleSignIn)}
+        action=""
+        className="w-full flex flex-col gap-4 mt-4"
+      >
+        <div className="w-full flex flex-col p-2 bg-white rounded border border-[#C0C7D4]">
+          <label htmlFor="username" className="text-[10px] text-[#687286]">
+            Nome de usuário
+          </label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Escreva aqui"
+            className="placeholder:text-sm placeholder:text-[#A8B1C2] outline-none"
+            {...register("username")}
+          />
+        </div>
+        <div className="w-full flex py-2 px-3 bg-white rounded border border-[#C0C7D4]">
+          <div className="w-full flex flex-col">
+            <label htmlFor="password" className="text-[10px] text-[#687286]">
+              Senha
+            </label>
+
+            <input
+              type={visible ? "text" : "password"}
+              id="password"
+              placeholder="Digite aqui a senha"
+              className="w-full placeholder:text-sm placeholder:text-[#A8B1C2] outline-none"
+              {...register("password")}
+            />
+          </div>
+          <button onClick={() => setVisible(!visible)}>
+            {visible ? <EyeSlash size={24} /> : <Eye size={24} />}
+          </button>
+        </div>
+        <Link to={"/recover-password"} className="text-xs text-[#1E3868]">
+          Esqueci minha senha
+        </Link>
+        <button
+          disabled={isSubmitDisable}
+          className="w-full flex justify-center py-2 bg-[#365DA5] text-white font-semibold rounded disabled:opacity-60"
         >
-            <div className="w-full flex flex-col p-2 bg-white rounded border border-[#C0C7D4]">
-                <label htmlFor="username"
-                    className='text-[10px] text-[#687286]'
-                >Nome de usuário</label>
-                <input
-                    type="text" id='username' placeholder='Escreva aqui'
-                    className='placeholder:text-sm placeholder:text-[#A8B1C2] outline-none'
-                    {...register('username')}
-                />
-            </div>
-            <div className="w-full flex py-2 px-3 bg-white rounded border border-[#C0C7D4]">
-                <div className="w-full flex flex-col">
-                    <label htmlFor="password"
-                        className='text-[10px] text-[#687286]'
-                    >Senha</label>
-                    
-                    <input 
-                        type={visible ? 'text' : 'password'} id='password' placeholder='Digite aqui a senha'
-                        className='w-full placeholder:text-sm placeholder:text-[#A8B1C2] outline-none'
-                        {...register('password')}
-                    />
-                </div>
-                <button onClick={() => setVisible(!visible)}>
-                    {
-                        visible 
-                        ? <EyeSlash size={24} />
-                        : <Eye size={24} />
-                    }
-                </button>
-            </div>
-            <Link to={'/recover-password'}
-                className='text-xs text-[#1E3868]'    
-            >Esqueci minha senha</Link>
-            <button disabled={isSubmitDisable} className="w-full flex justify-center py-2 bg-[#365DA5] text-white font-semibold rounded disabled:opacity-60">Entrar</button>
-        </form>
+          Entrar
+        </button>
+      </form>
     </>
   );
 }
--- src/pages/layouts/Authentication.tsx
+++ src/pages/layouts/Authentication.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Authentication from "../../assets/Authentication.svg"
-import Logo from '../../assets/Logo.svg'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Authentication from "../../assets/Authentication.svg";
+import Logo from "../../assets/Logo.svg";
 
 const AuthenticationLayout: React.FC = () => {
   return (
-    <div className='w-screen h-screen flex flex-col lg:flex-row justify-end'>
-        <div className="flex flex-col w-full justify-center items-center relative">
-        <div className="w-2/4 flex flex-col items-center">
-            <img src={Logo} 
-                className='mb-8 w-2/3'    
-                alt="Logo resolve assist"
-            />
-        <Outlet />
+    <div className="w-screen h-screen flex flex-col lg:flex-row justify-center">
+      <div className="flex flex-col w-full h-full justify-center items-center relative z-10">
+        <div className="flex flex-col items-center">
+          <img src={Logo} className="mb-8 w-2/3" alt="Logo resolve assist" />
+          <Outlet />
         </div>
-
-            <div className="flex w-full justify-center py-2 bg-[#D9DDE5] absolute bottom-0 text-[10px]">
-                {new Date().getFullYear()} - Powered by Ubicua ©
-            </div>
-        </div>
-        <img src={Authentication} 
-            alt='Imagem de uma pessoa enchendo o pneu'
-            className=''    
-        />
+        <footer className="flex w-full justify-center py-2 bg-[#D9DDE5] absolute bottom-0 text-[10px]">
+          {new Date().getFullYear()} - Powered by Ubicua ©
+        </footer>
+      </div>
+      <img
+        src={Authentication}
+        alt="Imagem de uma pessoa enchendo o pneu"
+        className="lg:static opacity-55 lg:opacity-100 absolute inset-0 z-0 object-cover w-full h-full lg:w-auto lg:h-auto"
+      />
     </div>
   );
-}
+};
 
 export { AuthenticationLayout };
+
