cb67012f17638fe7fa5827f2730b4b0f728c6461
Asaf Araujo <asaff.viana@outlook.com>
2024-10-17 18:52:43 -0300

    web.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	base: '/',
 	plugins: [react()],
 	preview: {
 		// defaultPort: 3000,
 		host: '0.0.0.0',
-		port: 5173
+		port: 5173,
 	},
 	resolve: {
 		alias: {
